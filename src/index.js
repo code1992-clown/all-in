9b3a94eedd8317dec6b308ad09f98734be740b12
code1992-clown <czfzxzyq@163.com>
2024-04-03 13:54:34 +0800

    f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// import './index.css'
+import '../static/css/index.css'
+const chat = require('../static/imgs/we_common.jpg')
+const img = new Image()
+img.className = 'object-cover w-2/4'
+img.src = chat
+
+const imgContainer = document.getElementById('img')
+imgContainer.appendChild(img)
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,37 +5,28 @@ module.exports = {
         main: './src/index.js'
     },
     mode: 'development',
-
-
     plugins: [
         new htmlWebpackPlugin({
             template: path.resolve(__dirname, 'src/index.html')
         }),
     ],
     module: {
-        generator:{
-            asset: {
-                publicPath: 'assets/',
-        
-                // 将静态资源输出到相对于 'output.path' 的指定文件夹中，webpack 5.67.0 后可用
-                outputPath: 'cdn-assets/',
-              }
-        },
         rules: [
             {
-                test:/\.css$/,
-                use:['style-loader','css-loader']
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             },
             {
-                test:/\.(png|jpg)$/,
-                type:'asset'
+                test: /\.(png|jpg)$/,
+                type: 'asset'
             },
-        
+
         ]
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'docs'),
+        assetModuleFilename: 'images/[hash][ext][query]',
         clean: true, // 在生成文件之前清空 output 目录
     }
 }
